64332a7e9b6da37dc92d125cfd99ab1f8a3cd516
Philip Wanderrienov <philip.wanderrienov@praweda.id>
2024-06-10 17:17:58 +0700

    update 10/06.2024
    - Changing the pie charts model
    - Inserting grid inside cards on skn dashboard (not done yet, still need to be fix)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MyServiceComponent } from './my-service/my-service.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
     HomeComponent,
     HeroComponent,
     AboutMeComponent,
+    MyServiceComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-service/my-service.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-service',
+  templateUrl: './my-service.component.html',
+  styleUrl: './my-service.component.css'
+})
+export class MyServiceComponent {
+
+}
